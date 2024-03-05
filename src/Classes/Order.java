976710b8ea23f04883b463fa2d7401da50cf647e
csmsstan <162117066+csmsstan@users.noreply.github.com>
2024-03-05 11:31:07 +0000

    Update class files

--- src/Classes/Order.java
+++ src/Classes/Order.java
@@ -1,6 +1,17 @@
-// SS
 
 package Classes;
+
+/**
+ * <h1> Order </h1>
+ * 
+ * The order class represents a customer's order, which can then be cooked and served.
+ * 
+ * @author Samuel Stanton
+ * @version 0.1
+ * @since 2024-03-04
+ */
+
+
 public class Order {
 	int orderNumber;
 	int customerNumber;
@@ -9,7 +20,13 @@ public class Order {
 	int orderCookTimeSeconds;
 	boolean isOrderCooked = false;
 	
-// Constructor for an order:
+/** Constructs a new order with a number, customer number, pizza to be ordered and quantity.
+ * 
+ * @param orderNumber The ID of the order. Should be auto-incremented when the order is created.
+ * @param customerNumber The ID of the customer who created the order.
+ * @param orderPizza The pizza to be ordered.
+ * @param pizzaQuantity The amount of pizzas that need to be cooked. 
+ */
 	
 	public Order(int orderNumber,
 				 int customerNumber,
@@ -22,7 +39,10 @@ public class Order {
 		this.orderCookTimeSeconds = orderPizza.pizzaCookTimeSeconds * pizzaQuantity;
 	}
 	
-// Serve an order, but only if it's cooked:
+/** Serves an order to the customer who ordered it, as long as the order has been cooked first.
+ * 
+ * @return A boolean representing if the order has been successfully served or not.
+ */
 	
 	public boolean serveOrder() {
 		if (!isOrderCooked) {
