7b863d0b7407ee2d55655e1db97704fe30085c57
csmsstan <162117066+csmsstan@users.noreply.github.com>
2024-04-09 10:42:27 +0100

    Removed unnecessary import

--- src/pizzafactory/pizzaFactorySimulationMain.java
+++ src/pizzafactory/pizzaFactorySimulationMain.java
@@ -4,7 +4,6 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.concurrent.PriorityBlockingQueue;
 
 import pizzafactory.classes.Customer;
 import pizzafactory.classes.Order;
